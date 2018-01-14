e8407489fd088b5c391e5d24a93d8ce13c4d8b10
Nicholas <2272750086@qq.com>
2018-01-14 09:23:57 +0800

    Update annually.swift

--- api/annually.swift
+++ api/annually.swift
@@ -3,5 +3,7 @@ import Foundation
 var books = [:]
 var journeys = [:]
 var exercises = []
+var films = []
 books["1th Jan"] = "Life and death in Shanghai"
 journeys["6th,7th Jan"] = "Huangshan"
+films.append("13th JAN, JUMANJI")
